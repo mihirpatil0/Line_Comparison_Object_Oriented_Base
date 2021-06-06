45843e833b028495f80fd27889a18e7b30d27958
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-07 00:42:17 +0530

    Using equal() method comparing line length.

--- LineComparison.java
+++ LineComparison.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LineComparison
 {
-	public float x1=0, x2=0, y1=0, y2=0, lengthOfLine;
+	public float x1=0, x2=0, y1=0, y2=0, a1=0, a2=0, b1=0, b2=0, firstLineLength, secondLineLength;
 	
 	/**
 	 * @param args
@@ -27,22 +27,43 @@ public class LineComparison
 	}
 	
 	/**
-	 * Calculating length of an line.
+	 * Calculating length of an two line.
 	 * using the Cartesian formula.
+	 * Comparing two lines length using equal() method.
 	 */
 	private void lineLength()
 	{
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter the value of a point x1:"); 
+		System.out.println("Enter the value of a point x1:"); 
 		x1 = scanner.nextFloat();
-		System.out.print("Enter the value of a point y1:"); 
+		System.out.println("Enter the value of a point y1:"); 
 		y1 = scanner.nextFloat();
-		System.out.print("Enter the value of a point x2:"); 
+		System.out.println("Enter the value of a point x2:"); 
 		x2 = scanner.nextFloat();
-		System.out.print("Enter the value of a point y2:"); 
+		System.out.println("Enter the value of a point y2:"); 
 		y2 = scanner.nextFloat();
-		lengthOfLine =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
-		System.out.println("The length of a line is : " + lengthOfLine);
+		System.out.println("Enter the value of a point a1:"); 
+		a1 = scanner.nextFloat();
+		System.out.println("Enter the value of a point b1:"); 
+		b1 = scanner.nextFloat();
+		System.out.println("Enter the value of a point a2:"); 
+		a2 = scanner.nextFloat();
+		System.out.println("Enter the value of a point b2:"); 
+		b2 = scanner.nextFloat();
+		float firstLineLength =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
+		float secondLineLength =(float) Math.sqrt( ( ( a2 - a1 ) * ( a2 - a1 ) ) + ( ( b2 - b1 ) * ( b2 - b1 ) ) );
+		System.out.println("The length of a line is:" + firstLineLength);
+		System.out.println("The length of a line is:" + secondLineLength);
+		Float firstLineLengthObj1 = firstLineLength;
+		Float secondLineLengthObj2 = secondLineLength;
+		if (firstLineLengthObj1.equals(secondLineLengthObj2))
+		{
+			System.out.println("The length of both the lines are equal.");
+		}
+		else
+		{
+			System.out.println("The length of both the lines are different.");
+		}
 		scanner.close();
 	}
 }
